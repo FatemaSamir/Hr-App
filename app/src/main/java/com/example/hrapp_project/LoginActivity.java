5c1bb2f674b07f0c28c2f862718f07c25209afd8
FatemaSamir <201627220@std.sci.cu.edu.eg>
2020-12-25 06:40:03 +0200

    logincommit_1

---
+++ app/src/main/java/com/example/hrapp_project/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.hrapp_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        register = findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:startActivity(new Intent(this,Register.class));
+            break;
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/hrapp_project/User.java
@@ -0,0 +1,60 @@
+package com.example.hrapp_project;
+
+public class User {
+
+    String full_name,Email,Department,Age,phone;
+
+    public User(String full_name, String email) {
+        this.full_name = full_name;
+        Email = email;
+
+    }
+    public User(String full_name, String email, String department, String age, String phone) {
+        this.full_name = full_name;
+        Email = email;
+        Department = department;
+        Age = age;
+        this.phone = phone;
+    }
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String age) {
+        Age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+}
