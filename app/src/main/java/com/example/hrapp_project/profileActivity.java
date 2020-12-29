289e3ca3a2505f16a829fab2998a7de14128f8cc
Samsung <esraa.ahmed.hamdy@gmail.com>
2020-12-29 22:56:50 +0200

    hr

--- app/src/main/java/com/example/hrapp_project/profileActivity.java
+++ app/src/main/java/com/example/hrapp_project/profileActivity.java
@@ -2,13 +2,28 @@ package com.example.hrapp_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class profileActivity extends AppCompatActivity {
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout=(Button) findViewById(R.id.signOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(profileActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
